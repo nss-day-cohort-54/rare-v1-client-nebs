6f60a72f5bdfaf78c58c2ea7542bf23437553559
Wil Wardrop <wwardrop@gmail.com>
2022-04-25 16:00:23 -0500

    adding tag form to tag list and created function in tag list

--- src/components/Tags/TagList.js
+++ src/components/Tags/TagList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { getAllTags, addTag } from "./TagManager";
+import { getAllTags, addTag, updateTag, addNewTag } from "./TagManager";
 import { useHistory } from "react-router-dom";
+import { TagForm } from "./TagForm";
 
 
 export const TagList = () => {
@@ -13,22 +14,66 @@ export const TagList = () => {
             .then((tag) => setTags(tag))
     }, [])
 
+    const handleChange = (evt) => {
+        new_tag = Object.assign({}, tag)
+
+        new_tag[evt.target.name] = evt.target.value 
+
+        setTag(new_tag)
+
+        print(new_tag)
+        
+    }
+
+    const onSubmitClick = (tagInfo) => {
+        if("id" in tagInfo){
+            updateTag(tagInfo)
+        }
+        else{
+            addNewTag(tagInfo)
+        }
+
+    }
 
 
 
     return (
         <>
+        <div className="tagList">
             {tags.map(tag => {
-                return <div className="card tag--list" key={`${tag.id}`}>
-                    <div className="tag--label">
-                        {tag.label}
+                    return <div className="card tag--list" key={`${tag.id}`}>
+                        <div className="tag--label">
+                            {tag.label}
+                        </div>
+                        <button className="btn edit-tag">Edit</button>
+                        <button className="btn delete-tag">Delete</button>
                     </div>
-                    <button className="btn edit-tag">Edit</button>
-                    <button className="btn delete-tag">Delete</button>
-                </div>
 
-            }
-            )}
+                }
+                )}
+
+        </div>
+
+        <div className="tagForm">
+            <div>
+                <label htmlFor="label">Enter Label Name</label>
+                <input
+                className="labelInput"
+                type="text"
+                name="label"
+                onChange={
+                    () => {
+                        handleChange
+                    }
+                }
+                />
+
+            </div>
+
+
+            <TagForm tag = {tag} setTag = {setTag} onSubmitClick = {onSubmitClick}/>
+        </div>
+           
         </>
     )
 }
