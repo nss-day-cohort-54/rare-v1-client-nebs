607fa04ffb3983d8557109292a1f91025bbb2f0c
Destiny Frith <desilyvahn@gmail.com>
2022-04-26 15:53:40 -0500

    tuesafternoon

--- src/components/Categories/CategoryManager.js
+++ src/components/Categories/CategoryManager.js
@@ -5,22 +5,23 @@ import { host } from "../Settings"
 
 export const get_all_categories = () => {
     return fetch(`${host}/categories`)
-    .then(res => res.json())
+        .then(res => res.json())
 }
 
 export const delete_category = (id) => {
-    
-    return fetch(`${host}/categories/${id}`, {method: "DELETE"})
+
+    return fetch(`${host}/categories/${id}`, { method: "DELETE" })
 }
 
 
 export const update_category = (categoryObject) => {
-    const fetchOptions = 
-        {method: "PUT",
-        headers:{
+    const fetchOptions =
+    {
+        method: "PUT",
+        headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(categoryObject)
-        }
+    }
     return fetch(`${host}/categories/${categoryObject.id}`, fetchOptions)
 }
